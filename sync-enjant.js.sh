f814cf225ead8848722b2bca76e0a8e2930348f1
Torsten Kinzel <torsten.kinzel@innoq.com>
2013-07-19 21:05:04 +0200

    deleted sync-Skript

--- sync-enjant.js.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rsync -avrz /Users/philipp/GIT/enchant.js/dev/ public/javascripts/
-rsync -avrz /Users/philipp/GIT/enchant.js/images/ public/images/
-#rsync -avrz /Users/philipp/GIT/enchant.js/dev/ public/javascripts/
-
