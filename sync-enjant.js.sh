c5150a96792108c6c6625b066e7de8f152f76e13
Philipp Haußleiter <philipp@haussleiter.de>
2013-07-19 20:56:36 +0200

    initial commit

---
+++ sync-enjant.js.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rsync -avrz /Users/philipp/GIT/enchant.js/dev/ public/javascripts/
+rsync -avrz /Users/philipp/GIT/enchant.js/images/ public/images/
+#rsync -avrz /Users/philipp/GIT/enchant.js/dev/ public/javascripts/
+
