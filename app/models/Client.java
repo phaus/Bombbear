f472e2dc7d4dcddc905bb1efffa9bfc13c64f052
Philipp Haußleiter <philipp@haussleiter.de>
2013-07-21 14:18:11 +0200

    adding start positions for all players

--- app/models/Client.java
+++ app/models/Client.java
@@ -1,11 +1,17 @@
 package models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Client {
 	public String username;
 	public String character;
-
-	public Client(String character, String username) {
+	public Map<String, Integer> start = new HashMap<String,Integer>();
+	
+	public Client(String character, String username, Integer[] sPos) {
 		this.username = username;
 		this.character = character;
+		this.start.put("x", sPos[0]);
+		this.start.put("y", sPos[1]);
 	}
 }
--- app/models/Game.java
+++ app/models/Game.java
@@ -9,6 +9,17 @@ public class Game {
 	private final static String[] CHARS = { "chara0", "chara1", "chara2",
 			"chara3", "chara4", "chara5", "chara6", "chara7" };
 
+	private final static Map<String, Integer[]> POSITIONS = new HashMap<String, Integer[]>();
+	static{
+		POSITIONS.put( "chara0", new Integer [] {32,32});
+		POSITIONS.put( "chara1", new Integer [] {64,32});
+		POSITIONS.put( "chara2", new Integer [] {32,64});
+		POSITIONS.put( "chara3", new Integer [] {64,64});
+		POSITIONS.put( "chara4", new Integer [] {32,48});
+		POSITIONS.put( "chara5", new Integer [] {48,32});
+		POSITIONS.put( "chara6", new Integer [] {48,48});
+		POSITIONS.put( "chara7", new Integer [] {0,0});
+	}
 	private Map<String, Client> players = new HashMap<String, Client>();
 	private int charCounter = 0;
 
@@ -24,7 +35,7 @@ public class Game {
 			String player = CHARS[charCounter];
 			Logger.info("Player " + username + " uses Char " + player);
 			charCounter++;
-			players.put(ip, new Client(player, username));
+			players.put(ip, new Client(player, username, POSITIONS.get(player)));
 			return player;
 		}
 		return null;
